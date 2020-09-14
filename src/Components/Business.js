f9f94b0ed2ba9e7cfcd0e2d4698d097b6b034f0e
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-14 15:01:29 +0200

    alt by immage

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -2,36 +2,37 @@ import React, { Component } from 'react';
 
 class Business extends Component {
     state = {
-        data : []
-      }
+        data: []
+    }
 
-      componentDidMount(){
-          fetch('https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=baa351b96cbf41aebb9eca53577868c8')
-          .then(Response => Response.json())
-          .then(json => {
-              this.setState({ data : json.articles});
-              console.log(this.state.data)
-          })       
-      }
-      
-      render() {
-          return(
-              <div className="divs">
+    componentDidMount() {
+        fetch('https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=baa351b96cbf41aebb9eca53577868c8')
+            .then(Response => Response.json())
+            .then(json => {
+                this.setState({ data: json.articles });
+                console.log(this.state.data)
+            })
+    }
 
-          {this.state.data.map((elt, i) =>
-          
-            <article className="articles" key={elt.id}>
-                <img src={elt.urlToImage}></img>
-                <h1>{elt.title}</h1>
-                <p>{elt.content}</p>
-                <a href={elt.url}>Read More</a>
-                <h6>{elt.publishedAt}</h6>
-                <p>{elt.author}</p>    
-            </article>)}
-          </div>
-          
-          )}
-          
+    render() {
+        return (
+            <div className="divs">
+
+                {this.state.data.map((elt, i) =>
+
+                    <article className="articles" key={elt.id}>
+                        <img src={elt.urlToImage} alt=""></img>
+                        <h1>{elt.title}</h1>
+                        <p>{elt.content}</p>
+                        <a href={elt.url}>Read More</a>
+                        <h6>{elt.publishedAt}</h6>
+                        <p>{elt.author}</p>
+                    </article>)}
+            </div>
+
+        )
+    }
+
 }
 
 export default Business;
--- src/Components/Technik.js
+++ src/Components/Technik.js
@@ -2,36 +2,37 @@ import React, { Component } from 'react';
 
 class Technik extends Component {
     state = {
-        data : []
-      }
+        data: []
+    }
 
-      componentDidMount(){
-          fetch('https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=baa351b96cbf41aebb9eca53577868c8')
-          .then(Response => Response.json())
-          .then(json => {
-              this.setState({ data : json.articles});
-              console.log(this.state.data)
-          })       
-      }
+    componentDidMount() {
+        fetch('https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=baa351b96cbf41aebb9eca53577868c8')
+            .then(Response => Response.json())
+            .then(json => {
+                this.setState({ data: json.articles });
+                console.log(this.state.data)
+            })
+    }
 
-      render() {
-          return(
-              <div className="divs">
-          
-          {this.state.data.map((elt, i) =>
-          
-            <article className="articles" key={elt.id}>
-                <img src={elt.urlToImage}></img>
-                <h1>{elt.title}</h1>
-                <p>{elt.content}</p>
-                <a href={elt.url}>Read More</a>
-                <h6>{elt.publishedAt}</h6>
-                <p>{elt.author}</p>    
-            </article>)}
-          </div>
-          
-          )}
-          
+    render() {
+        return (
+            <div className="divs">
+
+                {this.state.data.map((elt, i) =>
+
+                    <article className="articles" key={elt.id}>
+                        <img src={elt.urlToImage} alt=""></img>
+                        <h1>{elt.title}</h1>
+                        <p>{elt.content}</p>
+                        <a href={elt.url}>Read More</a>
+                        <h6>{elt.publishedAt}</h6>
+                        <p>{elt.author}</p>
+                    </article>)}
+            </div>
+
+        )
+    }
+
 }
 
 export default Technik;
